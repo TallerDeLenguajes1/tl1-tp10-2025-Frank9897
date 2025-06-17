2f0888e75827c3226b39ae3902bb0e9d41dbee88
Vasquez Franco <franco.vasquez9897@gmail.com>
2025-06-17 16:43:44 -0300

    Codificacion Ejercicio 2

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System.Text.Json;
+using esusuarios;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient cliente = new HttpClient();
+        string url = "https://jsonplaceholder.typicode.com/users";
+
+        HttpResponseMessage respuesta = await cliente.GetAsync(url);
+        respuesta.EnsureSuccessStatusCode();
+
+        string respuestaBody = await respuesta.Content.ReadAsStringAsync();
+        List<Usuarios.Root> usuarios = JsonSerializer.Deserialize<List<Usuarios.Root>>(respuestaBody);
+
+        foreach (var usuario in usuarios)
+        {
+            if (usuario.id < 6)
+            {
+                Console.WriteLine($"\n******Usuario {usuario.id}******\n");
+                Console.WriteLine($"Nombre: {usuario.name}\nCorreo: {usuario.email}\nDomicilio: {usuario.address}");
+            }
+        }
+                string guardarJson = JsonSerializer.Serialize(usuarios);
+                string json = Path.Combine(Directory.GetCurrentDirectory(),"usuarios.json");
+                await File.WriteAllTextAsync(json,guardarJson);
+        
+    }
+
+    
+}
