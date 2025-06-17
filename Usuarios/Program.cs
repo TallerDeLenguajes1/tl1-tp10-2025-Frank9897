ab303b12bcdc81dc631bc26edc0e05061a6c3f01
Vasquez Franco <franco.vasquez9897@gmail.com>
2025-06-17 16:52:07 -0300

    Subir el archivo para que se guarde solo 5 usuarios

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -12,17 +12,18 @@ class Program
 
         string respuestaBody = await respuesta.Content.ReadAsStringAsync();
         List<Usuarios.Root> usuarios = JsonSerializer.Deserialize<List<Usuarios.Root>>(respuestaBody);
-
+        List<Usuarios.Root> usu = new List<Usuarios.Root>();
         foreach (var usuario in usuarios)
         {
             if (usuario.id < 6)
             {
                 Console.WriteLine($"\n******Usuario {usuario.id}******\n");
                 Console.WriteLine($"Nombre: {usuario.name}\nCorreo: {usuario.email}\nDomicilio: {usuario.address}");
+                usu.Add(usuario);
             }
         }
     
-        string guardarJson = JsonSerializer.Serialize(usuarios);
+        string guardarJson = JsonSerializer.Serialize(usu);
         string json = Path.Combine(Directory.GetCurrentDirectory(),"usuarios.json");
         await File.WriteAllTextAsync(json,guardarJson);
 
