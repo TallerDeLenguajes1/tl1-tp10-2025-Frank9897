e98d20dd5c4425730a29cf0742644485a3f57c82
Vasquez Franco <franco.vasquez9897@gmail.com>
2025-06-27 16:52:08 -0300

    Codificacion Ejercicio 3

---
+++ MiWebApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using esapi;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient cliente = new HttpClient();
+        string url = "https://catfact.ninja/fact";
+        HttpResponseMessage respuesta = await cliente.GetAsync(url);
+        respuesta.EnsureSuccessStatusCode();
+
+        string respuestabody = await respuesta.Content.ReadAsStringAsync();
+        Root? randomgato = JsonSerializer.Deserialize<Root>(respuestabody);
+
+
+        if (randomgato != null)
+        {
+            Console.WriteLine($"Curiosidad: {randomgato.fact}\nLongitud: {randomgato.length}");
+
+            string guardarJson = JsonSerializer.Serialize(randomgato);
+            string json = Path.Combine(Directory.GetCurrentDirectory(), "gato.json");
+            await File.WriteAllTextAsync(json, guardarJson);
+        }
+        else
+        {
+            Console.WriteLine("Error al deserializar la respuesta.");
+        }
+    }
+
+}
---
+++ MiWebApi/gato.cs
@@ -0,0 +1,15 @@
+namespace esapi;
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Root
+    {
+        //[JsonProperty("fact")]
+        public string? fact { get; set; }
+
+        //[JsonProperty("length")]
+        public int? length { get; set; }
+    }
+
+
+
+
+
